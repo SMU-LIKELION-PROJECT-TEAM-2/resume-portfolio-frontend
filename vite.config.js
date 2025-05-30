5f28e2504f33afae1c2ee08a1210420834763b0a
crol0101@naver.com <crol0101@naver.com>
2025-05-30 12:44:25 +0900

    chore: vite 프로젝트로 마이그레이션

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: false,
+    fs: {
+      strict: false,
+    },
+    historyApiFallback: true, 
+  },
+  build: {
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
+});
