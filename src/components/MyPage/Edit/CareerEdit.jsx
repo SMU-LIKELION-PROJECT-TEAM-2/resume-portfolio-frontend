e0d4bed5d46dacf3ba436a0871542a8d9dabdac6
DaeHun Kang <crol0101@naver.com>
2025-06-25 17:33:19 +0900

    feat: 프로필 편집 디자인 추가

--- src/components/MyPage/Edit/CareerEdit.jsx
+++ src/components/MyPage/Edit/CareerEdit.jsx
@@ -1,11 +1,64 @@
 import styled from "@emotion/styled";
 
 const Layout = styled.section`
+  display: flex;
+  flex-direction: column;
   position: relative;
-  width: 790px;
-  height: 630px;
+  width: 710px;
   background-color: #eeeeee;
   border-radius: 8px;
+  padding: 40px;
+  gap: 40px;
+`;
+
+const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Detail = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Period = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const Text = styled.input`
+  height: 40px;
+  border: none;
+  border-radius: 10px;
+  font-size: larger;
+  padding-inline: 20px;
+`;
+
+const TextArea = styled.textarea`
+  border: none;
+  border-radius: 10px;
+  resize: none;
+  padding: 20px;
+  font-size: larger;
+`;
+
+const InputDate = styled.div`
+  flex: 1;
+  height: 62px;
+  background-color: white;
+  border-radius: 10px;
+`;
+
+const EditButton = styled.button`
+  background-color: #d9d9d9;
+  border-radius: 63px;
+  height: 54px;
+  width: 178px;
+  border: none;
+  align-self: flex-end;
+  font-size: 20px;
+  font-weight: 500;
+  cursor: pointer;
 `;
 
 const CareerEdit = () => {
@@ -15,12 +68,27 @@ const CareerEdit = () => {
         style={{
           position: "absolute",
           top: "-50px",
+          left: "0px",
           fontSize: "18px",
           fontWeight: "500",
         }}
       >
         나의 커리어
       </p>
+      <Title>
+        <h3>제목</h3>
+        <Text placeholder="ex) ㅁㅁ사 디자인 인턴" />
+      </Title>
+      <Detail>
+        <h3>업무 상세</h3>
+        <TextArea rows={6} placeholder="업무 내용을 적는 칸" />
+      </Detail>
+      <h3>업무 기간</h3>
+      <Period>
+        <InputDate></InputDate>
+        <InputDate></InputDate>
+      </Period>
+      <EditButton>수정 완료</EditButton>
     </Layout>
   );
 };
--- src/components/MyPage/Edit/DocumentEdit.jsx
+++ src/components/MyPage/Edit/DocumentEdit.jsx
@@ -1,34 +1,107 @@
 import styled from "@emotion/styled";
+import { Plus } from "lucide-react";
 const Layout = styled.section`
-  width: 790px;
+  width: 710px;
   height: 610px;
   background-color: #eeeeee;
   border-radius: 8px;
+  padding: 40px;
+  display: flex;
+  gap: 120px;
 `;
-const ProfileImg = styled.div``;
+const ImgBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+  width: 185px;
+  align-items: center;
+`;
 
-const ProfileForm = styled.div``;
+const ProfileImg = styled.img`
+  height: 145px;
+  width: 145px;
+  border-radius: 100%;
+  background-color: #d0d0d0;
+`;
 
-const TextBox = styled.div``;
+const StackBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+const ProfileStack = styled.p`
+  margin: 0;
+  display: flex;
+  background-color: #d9d9d9;
+  height: 34px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 38px;
+`;
 
-const TextArea = styled.textarea``;
+const PlusButton = styled.button`
+  margin: 0;
+  display: flex;
+  height: 34px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 38px;
+  border: 1px dotted;
+`;
+
+const ProfileForm = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+const Text = styled.input`
+  border-radius: 10px;
+  border: none;
+  height: 50px;
+  padding-inline: 10px;
+  font-size: 16px;
+`;
+const TextArea = styled.textarea`
+  resize: none;
+  border-radius: 10px;
+  border: none;
+  padding: 10px;
+  font-size: 20px;
+`;
 
 const DocumentEdit = () => {
   return (
     <Layout>
-      <ProfileImg />
+      <ImgBox>
+        <ProfileImg />
+        <StackBox>
+          <ProfileStack>UIUX</ProfileStack>
+          <ProfileStack>BX</ProfileStack>
+          <PlusButton>
+            <Plus size={16} />
+          </PlusButton>
+        </StackBox>
+      </ImgBox>
       <ProfileForm>
         <TextBox>
           이름
-          <TextArea />
+          <Text placeholder="NAME" />
         </TextBox>
         <TextBox>
           직업
-          <TextArea />
+          <Text placeholder="ex) UIUX 디자이너" />
         </TextBox>
         <TextBox>
           자기소개
-          <TextArea />
+          <TextArea rows={14} placeholder="본인에 대한 설명을 쓰는 칸입니다." />
         </TextBox>
       </ProfileForm>
     </Layout>
