a6af27da80619965fef94d557af027267e1ecc9b
crol0101@naver.com <crol0101@naver.com>
2025-07-01 14:17:53 +0900

    design: css 기본 설정 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routes/Router";
-import { Global, css } from "@emotion/react";
 
-const GlobalStyle = css`
-  html,
-  body,
-  #root {
-    margin: 0;
-    padding: 0;
-    font-family: sans-serif;
-  }
-`;
 function App() {
   return (
     <BrowserRouter>
-      <Global styles={GlobalStyle} />
       <Router />
     </BrowserRouter>
   );
